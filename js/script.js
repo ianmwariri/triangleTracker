2de23b29c35cfdeb396abad8bd04e5c1541aa6c4
smokeyice <mwangi.ian3@gmail.com>
2018-08-10 11:38:43 +0300

    Add HTML Elements

---
+++ js/script.js
@@ -0,0 +1,20 @@
+function getTriangle() {
+
+  var a = parseFloat(prompt("side a"));
+  var b = parseFloat(prompt("side b"));
+  var c = parseFloat(prompt("side c"));
+
+  if (a === b && a === c && b === c) {
+    alert("That's an Equilateral Triangle!");
+  }
+  else if (a === b || a === c || b === c) {
+    alert("That's an Isosceles Triangle");
+  }
+  else if ((a + b) <= c || (a + c) <= b || (b + c) <= a) {
+    alert("Invalid Dimensions. NOT a Triangle");
+  }
+  else {
+    alert("Scalar");
+  }
+}
+getTriangle();
